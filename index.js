65fe92ad146b9836bfff55ab080f66fdb5388e67
Juan Carlos Funes Machicado <89326426+juancarlosfunesmachicado@users.noreply.github.com>
2024-04-09 18:17:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.json({ message: 'Hello, world!' });
+    res.json({ message: 'Hello' });
 });
 
 const port = process.env.PORT || 3000;
